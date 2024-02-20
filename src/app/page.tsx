b96b4a31caf2551c3de6ce61cc33737f4dde04bc
aayushker <singhaayushker@gmail.com>
2024-02-20 23:09:55 +0530

    Made the footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
+"use client" 
 import React from 'react';
 import NavBar from './components/navbar/navbar';
+// import Footer from './components/navbar/footer';
+import {NextUIProvider} from "@nextui-org/react";
 
 export default function Home() {
   return (
     <>
       <NavBar />
       <p>Hello</p>
-    </>
+      </>
   );
 }
