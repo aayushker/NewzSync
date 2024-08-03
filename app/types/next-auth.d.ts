8c1d539257d69726e07070bf937f8a50595799ea
aayushker <singhaayushker@gmail.com>
2024-08-04 02:02:37 +0530

    Updated the auth

---
+++ app/types/next-auth.d.ts
@@ -0,0 +1,9 @@
+// src/types/next-auth.d.ts
+
+import NextAuth from 'next-auth';
+
+declare module 'next-auth' {
+  interface Session {
+    userId: string;
+  }
+}
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -6,11 +6,22 @@ import { db } from '@/app/firebaseConfig';
 export default NextAuth({
   providers: [
     GoogleProvider({
-      clientId: `${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}`,
-      clientSecret: `${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET}`,
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET!,
     }),
   ],
   adapter: FirestoreAdapter(db),
+  cookies: {
+    sessionToken: {
+      name: `__Secure-next-auth.session-token`,
+      options: {
+        httpOnly: true,
+        sameSite: 'lax',
+        path: '/',
+        secure: process.env.NODE_ENV === 'production',
+      },
+    },
+  },
   callbacks: {
     async signIn({ user, account, profile }) {
       try {
@@ -33,13 +44,13 @@ export default NextAuth({
     async redirect({ url, baseUrl }) {
       return baseUrl; // Redirect to the home page after successful login
     },
-    async session({ session, user }) {
-      session.userId = user.id;
+    async session({ session, token }) {
+      session.userId = token.sub!;
       return session;
     },
   },
   pages: {
-    signIn: '/auth/signin', // Ensure this path exists and is accessible
+    signIn: '/pages/index.tsx',
   },
   debug: process.env.NODE_ENV === 'development',
 });
